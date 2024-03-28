086c7e9d093798ebbff44583f9213faed9df5001
mubashir-cvr <mubashircheruvayoor@gmail.com>
2024-03-28 14:22:33 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const express = require("express");
+
+const app = express();
+app.use((req, res, next) => {
+    console.log('In the Middleware !');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('Another Middleware !');
+    res.send("<h1>Hello From Express </h1>")
+});
+const server = http.createServer(app);
+
+server.listen(3000);
