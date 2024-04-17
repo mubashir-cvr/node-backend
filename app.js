636ca61f34a475fd0e59ab99365f608f4b67856d
mubashir-cvr <mubashircheruvayoor@gmail.com>
2024-04-17 17:40:12 +0530

    Changes

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const { Permission, Role } = require("./models/role");
 const bcrypt = require('bcrypt');
 const appRoutes = require("./routes/v1/index");
 const app = express();
-const cors = require('cors');
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
@@ -22,9 +21,6 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
-app.use(cors({
-  origin: 'https://marjintech.online'
-}));
 
 app.use("/api", appRoutes);
 
